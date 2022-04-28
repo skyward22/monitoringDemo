a77238bf2e8097218fcee6d9985e3599755b976e
Skyler Trent <skyler.trent93@gmail.com>
2022-04-28 14:13:06 -0600

    demoSetup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const path = require('path')
+const Rollbar = require('rollbar')
 
 const app = express()
+
+app.use(express.json())
+app.use('/style', express.static('./public/styles.css'))
+
+let students = []
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
@@ -14,11 +20,86 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
+    rollbar.info('html file served successfully.')
 })
+
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+    
+    const index = students.findIndex(studentName=> studentName === name)
+    
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+    
+})
+
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log(`Take us to warp ${port}!`))
 
+
+
+// const express = require('express')
+// const path = require('path')
+// const Rollbar = require('rollbar')
+
+// let rollbar = new Rollbar({
+//     accessToken: 'POST_SERVER_ITEM_ACCESS_TOKEN',
+//     captureUncaught: true,
+//     captureUnhandledRejections: true
+// })
+
+// const app = express()
+
+// app.use(express.json())
+// app.use('/style', express.static('./public/styles.css'))
+
+// let students = []
+
+// app.get('/', (req,res) => {
+//     res.sendFile(path.join(__dirname, '/public/index.html'))
+//     rollbar.info('html file served successfully.')
+// })
+
+// app.post('/api/student', (req, res)=>{
+//     let {name} = req.body
+//     name = name.trim()
+
+//     const index = students.findIndex(studentName=> studentName === name)
+
+//     if(index === -1 && name !== ''){
+//         students.push(name)
+//         rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+//         res.status(200).send(students)
+//     } else if (name === ''){
+//         rollbar.error('No name given')
+//         res.status(400).send('must provide a name.')
+//     } else {
+//         rollbar.error('student already exists')
+//         res.status(400).send('that student already exists')
+//     }
+
+// })
+
+// const port = process.env.PORT || 4545
+
+// // rolly
+// app.use(rollbar.errorHandler())
+
+// app.listen(port, () => console.log(`Take us to warp ${port}!`))
